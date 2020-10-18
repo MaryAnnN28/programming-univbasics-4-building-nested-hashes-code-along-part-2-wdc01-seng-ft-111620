513e0cb353468fa843567e8c67fda482b60a7b03
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-18 02:17:07 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -28,6 +28,7 @@ def update_status
   epic_tragedy[:capulet][:heroine][:status] = "dead".object_id
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   def update_status(epic_tragedy)
+    binding.pry
     puts epic_tragedy[:montague][:hero][:status]
     puts epic_tragedy[:capulet][:heroine][:status]
 binding.pry
